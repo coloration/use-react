24017aa0c77ae3dda3129a535aae743cc05aa079
WangBinyu <406279919@qq.com>
2019-10-22 00:47:46 +0800

    i18n

---
+++ packages/i18n/index.tsx
@@ -0,0 +1,89 @@
+import React, {
+  createContext,
+  useEffect,
+  useState,
+  SFC,
+  useContext,
+} from 'react'
+import { useLocalStorage } from './useLocalStorage'
+
+export enum I18nLanguages {
+  ZH_CN = 'zh-CH',
+  EN_US = 'en-US',
+}
+
+export type I18nLocales = {
+  [key: string]: string | { [key: string]: string }
+}
+
+export type I18nContextProps = {
+  lang?: string,
+  setLang: (lang: I18nLanguages) => void,
+  locales: I18nLocales,
+}
+
+export const I18nContext = createContext<Partial<I18nContextProps>>({})
+
+function defLang () {
+  let defaultLang: I18nLanguages
+  if (window.navigator) {
+    const { language, languages } = window.navigator
+    defaultLang = (
+      language ||
+      languages && languages.length && languages[0] ||
+      I18nLanguages.ZH_CN
+    ) as I18nLanguages
+  }
+  else {
+    defaultLang = I18nLanguages.ZH_CN
+  }
+
+  return defaultLang
+}
+
+export const I18nProvider: SFC<{
+  lang?: I18nLanguages,
+  locales?: I18nLocales,
+  storageKey?: string
+}> = ({
+  children,
+  lang: propLang,
+  locales: propLocales,
+  storageKey
+}) => {
+  const [lang, setLang] = useLocalStorage(
+    storageKey || 'lang',
+    propLang || defLang()
+  )
+
+  return (
+    <I18nContext.Provider value={{
+      lang, setLang, locales: propLocales
+    }}>
+      { children }
+    </I18nContext.Provider>
+  )
+}
+
+export const useLocale = (locales?: I18nLocales) => {
+  const { lang, locales: rootLocales } = useContext(I18nContext)
+  const currentLocales = locales || rootLocales
+
+  if (!currentLocales)
+    throw new Error('please set a root locales or component locales at least')
+
+  const [locale, setLocale] = useState(currentLocales[lang])
+
+  useEffect(() => {
+    setLocale(currentLocales[lang])
+  }, [lang])
+  
+  return [locale, setLocale]
+}
+
+export const useLang = () => {
+  const { lang, setLang } = useContext(I18nContext)
+  return [lang, setLang]
+}
+
+export { useLocalStorage }
